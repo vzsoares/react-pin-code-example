6904dd9a1f6d4b2f31a43d913cf9c451c462532f
vzsoares <jogolots2@gmail.com>
2022-08-29 23:25:06 -0300

    feat pin input

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [PIN, setPIN] = useState("");
+  const pinSize = 5;
+  function handleKeyDown(e) {
+    if (e.key === "Backspace") setPIN(PIN.slice(0, -1));
+    else if (PIN.length + 1 > pinSize) return;
+    else setPIN(PIN + e.key[0]);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div style={{ display: "flex", gap: "1rem" }}>
+          {[...new Array(pinSize)].map((_, i) => (
+            <input
+              value={PIN[i] ?? ""}
+              onKeyDown={handleKeyDown}
+              autoFocus={() => PIN[i] !== undefined}
+              maxLength="1"
+              style={{
+                width: "5rem",
+                height: "5rem",
+                fontSize: "2rem",
+                caretColor: "transparent",
+                textAlign: "center",
+                outline: `${PIN[i] !== undefined ? "4px solid orange":"0"}`,
+              }}
+            />
+          ))}
+        </div>
       </header>
     </div>
   );
